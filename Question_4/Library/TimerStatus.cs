dc569141416154d15af99bfd5e45f3b740efb2f2
Deank71 <Deank71@live.com>
2020-08-29 12:00:57 -0400

    Created Question 4 application

---
+++ Question_4/Library/TimerStatus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Question_4.Library
+{
+    public class TimerStatus
+    {
+
+        private int invokeMs;
+        private int maxMs;
+
+        public TimerStatus(int ms)
+        {
+            invokeMs = 0;
+            maxMs = ms;
+        }
+        public void Check(Object Info)
+        {
+            AutoResetEvent autoEvent = (AutoResetEvent)Info;
+            Console.WriteLine("{0} Status {1,2}.",
+                DateTime.Now.ToString("h:mm:ss.fff"),
+                (++invokeMs).ToString() + "/" + maxMs);
+
+            if (invokeMs == maxMs)
+            {
+                // Reset the counter and signal the waiting thread.
+                invokeMs = 0;
+                autoEvent.Set();
+            }
+        }
+    }
+}
+
---
+++ Question_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using Question_4.Library;
+using System;
+using System.Threading;
+
+namespace Question_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var timerEvent = new AutoResetEvent(false);
+
+            var timerSatus = new TimerStatus(200);
+
+            Console.WriteLine("Start timer.\n",
+                              DateTime.Now);
+            var stateTimer = new Timer(timerSatus.Check,
+                                       timerEvent, 2000, 10);
+            timerEvent.WaitOne();
+            stateTimer.Dispose();
+            Console.WriteLine("\nDone.");
+        }
+    }
+}
